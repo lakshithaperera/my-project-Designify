392b94e770f194f6c5c0b1f73e78f12e34bb4d53
lakshithaperera <pasindulakshitha33@gmail.com>
2022-07-13 11:52:57 +0530

    Updates

---
+++ precache-manifest.6ed45cfd782c117f8c5bee149628c710.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1dca4707607a034081c031f6b3a2647",
+    "url": "/my-project-Designify/index.html"
+  },
+  {
+    "revision": "431c32762e5d9f706d9c",
+    "url": "/my-project-Designify/static/js/2.5fa0a62f.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/my-project-Designify/static/js/2.5fa0a62f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8598d86b07f1822bc27f",
+    "url": "/my-project-Designify/static/js/main.d93de051.chunk.js"
+  },
+  {
+    "revision": "f9509d6de68a2293a3fb",
+    "url": "/my-project-Designify/static/js/runtime-main.28b6771c.js"
+  }
+]);
